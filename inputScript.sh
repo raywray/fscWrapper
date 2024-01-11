fe98efbe2881600bf02672b10d36fb6de5e04e75
ChabbyTMD <greena.mugoya@gmail.com>
2024-01-11 22:07:17 +0000

    Added est and tpl generation scripts

--- inputScript.sh
+++ inputScript.sh
@@ -12,8 +12,14 @@ MUT_RATE=6e-8
 # if NUM_OF_GROUPS = 2, #t = 1
 # if NUM_OF_GROUPS = 3, #t = 4
 
-if [ $NUM_OF_GROUPS -eq 3 ]; then
-  NUM_OF_TOPOLOGIES=4
+if [ $NUM_OF_GROUPS -eq 1 ]; then
+  NUM_OF_TOPOLOGIES=1
+  cd Panmixia/t1/
+  cp pre_template.est t1.est
+  cp pre_template.tpl t1.tpl
+  sed /s/"TDIV_PLACEHOLDER"/$TDIV/ t1.est
+  sed /s/"TPLUS01_PLACEHOLDER"/$TPLUS01/ t1.tpl
+
 else
   NUM_OF_TOPOLOGIES=1
 fi
