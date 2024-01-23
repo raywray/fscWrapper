839e75fd72c5870ad2983bcc2c2bf6764f5d4a0d
Trevor Mugoya <greena.mugoya@gmail.com>
2024-01-23 13:50:56 -0800

    Added functions for divergence events

--- InputPythonScript.py
+++ InputPythonScript.py
@@ -1,3 +1,5 @@
+import random
+
 NUM_OF_GROUPS=1
 TDIV=0
 TPLUS01=0
@@ -71,6 +73,54 @@ def oldest_migration_matrix(num_populations):
     oldest_matrix = [oldest_matrix] + [" ".join(row) for row in temp]
     return oldest_matrix
 
+def randomize_divergence_order(root_population_indices, leaf_population_indices, *args):
+    migrants = 1
+    growth_rate = 0
+    output = []
+    possible_roots = root_population_indices.copy()
+    possible_leaves = leaf_population_indices.copy()
+    rev_migration_matrix_index = len(leaf_population_indices)
+
+    while len(possible_leaves) > 0:
+        root = random.sample(possible_roots, 1)
+        offshoot = random.sample(possible_leaves, 1)
+        root_name = population_name(index=root)
+        offshoot_name = population_name(index=offshoot)
+        time = f"TDIV_{root_name}to{offshoot_name}"
+        new_deme_size = f"RES_{root_name}to{offshoot_name}"
+        output.append(
+            " ".join(map(str, [time, offshoot, root, migrants, new_deme_size, growth_rate, rev_migration_matrix_index]))
+        )
+        possible_roots.append(offshoot)
+        possible_leaves.remove(offshoot)
+        rev_migration_matrix_index -= 1
+
+    return output
+
+# # Assuming population_name function is defined elsewhere
+# def population_name(index):
+#     # Add your implementation of population_name
+#     pass
+
+
+def random_initializations():
+    add_ghost = random.choice([True, False])
+    split_SF = random.choice([True, False])
+    num_pop = 3 if split_SF else 2
+    num_pop += int(add_ghost)
+    w_index = num_pop - (2 if add_ghost else 1)
+    roots = [w_index]
+    leaves = list(range(w_index))
+    if add_ghost:
+        ghost_role = random.choice(["root", "leaf"])
+        if ghost_role == "root":
+            roots.append(w_index+1)
+        else:
+            leaves.append(w_index+1)
+    divergence_events = randomize_divergence_order(roots, leaves, split_SF=split_SF, ghost_present=add_ghost)
+    historical_events = divergence_events
+
+    return 0
 
 # TODO modify this function when the time comes
 def population_name(index):
