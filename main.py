0a86221186299dedf8ce190cdbb1bd052c6083e6
Raya Esplin <resplin5072@sdsu.edu>
2024-03-04 15:00:49 -0800

    deals with more previously hard-coded variables

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from pipeline_modules import generate_random_tpl, generate_random_est
 
 # User Provided Input
-MUTATION_RATE_MIN = 1e-7
-MUTATION_RATE_MAX = 1e-9
 NUM_POPS = 3
 SAMPLE_SIZES = [2, 4, 4]
+MUTATION_RATE_DIST = {"min": 1e-7, "max": 1e-9, "type": "unif"}
+EFFECTIVE_POP_SIZE_DIST = {"min": 100, "max": 300000, "type": "unif"}
+RESIZED_DIST = {"min": 0, "max": 100, "type": "unif"}
+ADMIX_DIST = {"min": 0, "max": 0.25, "type": "unif"}
 
-tpl_filename = "test2.tpl"
-est_filename = "test2.est"
+tpl_filename = "random.tpl"
+est_filename = "random.est"
 
-generate_random_tpl.generate_random_tpl_parameters(tpl_filename, NUM_POPS, SAMPLE_SIZES)
-generate_random_est.create_est(tpl_filename, est_filename, mutrate_min=MUTATION_RATE_MIN, mutrate_max=MUTATION_RATE_MAX)
+# generate_random_tpl.generate_random_tpl_parameters(tpl_filename, NUM_POPS, SAMPLE_SIZES)
+generate_random_est.create_est(
+    tpl_filename, 
+    est_filename, 
+    mutation_rate_dist=MUTATION_RATE_DIST,
+    ne_dist=EFFECTIVE_POP_SIZE_DIST,
+    resized_dist=RESIZED_DIST,
+    admix_dist=ADMIX_DIST
+)
 
 # command to run fsc
 # fsc27093 -t LD-pruned_SNPs.tpl -e LD-pruned_SNPs.est -m -n 10000 -L 50 -s 0 -M
--- pipeline_modules/generate_random_est.py
+++ pipeline_modules/generate_random_est.py
@@ -2,7 +2,7 @@ import re
 from itertools import chain
 
 
-def estimation(simple_params, complex_params, mutrate_min=0, mutrate_max=0):
+def estimation(simple_params, complex_params, mutation_rate_dist):
     return (
         [
             "// Priors and rules file",
@@ -10,7 +10,7 @@ def estimation(simple_params, complex_params, mutrate_min=0, mutrate_max=0):
             "",
             "[PARAMETERS]",
             "//#isInt? #name #dist. #min #max",
-            f"0 MUTRATE unif {mutrate_min} {mutrate_max} output",
+            f"0 MUTRATE {mutation_rate_dist["type"]} {mutation_rate_dist["min"]} {mutation_rate_dist["max"]} output",
         ]
         + [param for param in simple_params]
         + [
@@ -22,13 +22,10 @@ def estimation(simple_params, complex_params, mutrate_min=0, mutrate_max=0):
     )
 
 
-def get_population_parameters(input_template):
+def get_population_parameters(input_template, ne_dist):
     population_parameters = [line for line in input_template if "NPOP_" in line]
-    formatted_number = "{:,}".format(30 * 10**4).replace(
-        ",", ""
-    )  # TODO I think this should be user given?
     formatted_population_parameters = [
-        "1 {} unif 100 {} output".format(param, formatted_number)
+        "1 {} {} {} {} output".format(param, ne_dist["type"], ne_dist["min"], ne_dist["max"])
         for param in population_parameters
     ]
     return formatted_population_parameters
@@ -74,25 +71,26 @@ def get_parameter_pattern(prefix):
     return rf"{prefix}_[a-zA-Z0-9]+"
 
 
-def resize_parameters(input_template):
+def resize_parameters(input_template, resized_dist):
     resized_parameters = []
     if any("RES_" in line for line in input_template):
         resize_parameters = set(
             re.findall(get_parameter_pattern("RES"), " ".join(input_template))
         )
         resized_parameters = [
-            "0 {} unif 0 100 output".format(param)
-            for param in resize_parameters  # TODO is the 100 hardcoded?
+            "0 {} {} {} {} output".format(param, resized_dist["type"], resized_dist["min"], resized_dist["max"])
+            for param in resize_parameters
         ]
     return resized_parameters
 
 
-def get_admixture_parameters(input_template):
+def get_admixture_parameters(input_template, admix_dist):
     admixture_parameters = set(
         re.findall(get_parameter_pattern("a"), " ".join(input_template))
     )
     formatted_admixture_parameters = [
-        "0 {} unif 0 0.25 output".format(param) for param in admixture_parameters
+        "0 {} {} {} {} output".format(param, admix_dist["type"], admix_dist["min"], admix_dist["max"]) 
+        for param in admixture_parameters
     ]
     return formatted_admixture_parameters
 
@@ -113,7 +111,14 @@ def get_time_parameters(input_template):
 
 
 # this function generates an estimation file for a tpl template
-def create_est(input_template_filepath, est_filename="random.est", **kwargs):
+def create_est(
+        input_template_filepath, 
+        est_filename="random.est", 
+        mutation_rate_dist={}, 
+        ne_dist={},
+        resized_dist={},
+        admix_dist={}
+):
     input_template = []
 
     with open(input_template_filepath, "r") as inFile:
@@ -125,13 +130,13 @@ def create_est(input_template_filepath, est_filename="random.est", **kwargs):
     complex_parameters = []
 
     # Population parameters
-    simple_parameters.extend(get_population_parameters(input_template))
+    simple_parameters.extend(get_population_parameters(input_template, ne_dist))
 
     # Migration rate parameters
     simple_parameters.extend(get_migration_parameters(input_template))
 
     # Resizing parameters
-    simple_parameters.extend(resize_parameters(input_template))
+    simple_parameters.extend(resize_parameters(input_template, resized_dist))
 
     # Time parameters
     time_parameters = get_time_parameters(input_template)
@@ -150,10 +155,10 @@ def create_est(input_template_filepath, est_filename="random.est", **kwargs):
             )
 
     # Admixture parameters
-    simple_parameters.extend(get_admixture_parameters(input_template))
+    simple_parameters.extend(get_admixture_parameters(input_template, admix_dist))
 
     # Combine parameters & write to a file
-    est = estimation(simple_parameters, complex_parameters, **kwargs)
+    est = estimation(simple_parameters, complex_parameters, mutation_rate_dist)
     with open(est_filename, "w") as file:
         for line in est:
             file.write(line + "\n")
