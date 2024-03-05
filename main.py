a6b1e5a8918660f959b3d71e30f76f1be3e3b7ff
Raya Esplin <resplin5072@sdsu.edu>
2024-03-04 16:18:43 -0800

    sets up for the loop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from pipeline_modules import generate_random_tpl, generate_random_est
 
 # User Provided Input
@@ -18,22 +19,39 @@ TIME_DIST = {
     "extra_min": 0,
 }
 
-# Define file names
-tpl_filename = "testing_again.tpl"
-est_filename = "testing_again.est"
-
-# Generate random tpl and est files
-generate_random_tpl.generate_random_tpl_parameters(tpl_filename, NUM_POPS, SAMPLE_SIZES)
-generate_random_est.create_est(
-    tpl_filename,
-    est_filename,
-    mutation_rate_dist=MUTATION_RATE_DIST,
-    ne_dist=EFFECTIVE_POP_SIZE_DIST,
-    resized_dist=RESIZED_DIST,
-    admix_dist=ADMIX_DIST,
-    time_dist=TIME_DIST,
-    mig_dist=MIGRATION_DIST
-)
-
 # command to run fsc
 # fsc27093 -t LD-pruned_SNPs.tpl -e LD-pruned_SNPs.est -m -n 10000 -L 50 -s 0 -M
+
+# Create a loop, put output into folders
+# Create output directory
+if not os.path.exists("output"):
+    os.mkdir("output")
+for i in range(1, 11):
+    # make & move to directory 
+    output_folder_name = f"output/run_{i}"
+    os.mkdir(output_folder_name)
+    os.chdir(output_folder_name)
+
+    # TODO move other output files (vcf, sfs, etc.) to this directory
+
+    # Create filenames
+    tpl_filename = f"random_{i}.tpl"
+    est_filename = f"random_{i}.est"
+
+    # Generate random tpl & est files
+    generate_random_tpl.generate_random_tpl_parameters(tpl_filename, NUM_POPS, SAMPLE_SIZES)
+    generate_random_est.create_est(
+        tpl_filename,
+        est_filename,
+        mutation_rate_dist=MUTATION_RATE_DIST,
+        ne_dist=EFFECTIVE_POP_SIZE_DIST,
+        resized_dist=RESIZED_DIST,
+        admix_dist=ADMIX_DIST,
+        time_dist=TIME_DIST,
+        mig_dist=MIGRATION_DIST
+    )
+
+    # TODO run fsc
+
+    # go back to root directory
+    os.chdir("../..")
