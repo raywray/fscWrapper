2b126798e709bb9cc61620096e020dcfa180b281
Raya Esplin <resplin5072@sdsu.edu>
2024-05-29 15:50:20 -0700

    moves best fit model into its own folder

--- pipeline_modules/run_bootstrap_analysis.py
+++ pipeline_modules/run_bootstrap_analysis.py
@@ -1,4 +1,26 @@
 import os
+import re
+
+def extract_best_run(output_dir, prefix):
+    best_lhoods_txt_path = os.path.join(output_dir, "best_lhoods_results.txt")
+    with open(best_lhoods_txt_path, "r") as f:
+        first_line = f.readline().strip()
+    best_run_pattern = re.compile(r'(\d+):(\d+)')
+    match = best_run_pattern.search(first_line)
+    if match:
+        simulation_group = match.group(1)
+        run = match.group(2)
+    else:
+        print("No match found")
+    
+    new_best_run_folder = os.path.join(output_dir, "best_run")
+    
+    if not os.path.exists(new_best_run_folder):
+        os.makedirs(new_best_run_folder)
+        path_to_best_run = os.path.join(output_dir, f"{prefix}_run_{simulation_group}_output/run_{run}")
+        os.system(f"cp -r {path_to_best_run}/* {new_best_run_folder}")
+    
+    return new_best_run_folder
 
 def model_comp_with_aic(path_to_best_max_est_run, prefix):
     # the folder in the path must have the .bestlhoods and the .est (might have to move the .est in manually)
@@ -36,8 +58,8 @@ def visualize_best_fit_model(path_to_best_run, prefix):
     os.system(f"cp {tpl_file_path} {best_run_prefix_folder}")
 
     # move .obs to folder
-    # obs_file_path = os.path.join(path_to_best_run, f"{prefix}*.obs")
-    # os.system(f"cp {obs_file_path} {best_run_prefix_folder}")
+    obs_file_path = os.path.join(path_to_best_run, f"{prefix}*.obs")
+    os.system(f"cp {obs_file_path} {best_run_prefix_folder}")
 
     # define paths
     path_to_sfs_tools = "/Users/raya/Documents/School/fscWrapper/utilities/SFStools.r"
@@ -53,8 +75,10 @@ def visualize_best_fit_model(path_to_best_run, prefix):
 
     return
 
-max_est_run_path = "/Users/raya/Documents/School/fscWrapper/output/fsc_output/hops_run_69_output/run_3"
+
+output_dir = "/Users/raya/Documents/School/fscWrapper/output/fsc_output"
 prefix = "hops"
+max_est_run_path = extract_best_run(output_dir, prefix)
 
 # model_comp_with_aic(max_est_run_path, prefix)
 visualize_best_fit_model(max_est_run_path, prefix)
